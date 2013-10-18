f5b02f451c2c7f613047bb5eacb04b76f6c283d7
Naomi <neomi_kr@walla.com>
2013-10-18 09:44:24 +0300

    vghgfvjgjg

--- IsraKotzApp/HotelSearcher.py
+++ IsraKotzApp/HotelSearcher.py
@@ -14,14 +14,17 @@ def getHotelData(country, city):
     #Define the SQL query that will be used in YQL. This will return us the price of 1 pair of Nike Shoes.
     #query = "select * from html where url='" + URL + "' and xpath='//p[text()=\"1 Pair of Nike Shoes\"]/../following-sibling::td[1]/p'"
 
-    query = "select * from html where url='" + URL + "' and xpath=''//table[contains(@class, \"hotel_outline_box\")]/tr[5]/td[2]/p'"
-
+    query1 = "select * from html where url='" + URL + "' and xpath='//table[contains(@class, \"hotel_outline_box\")]/tr[5]/td[2]/p'"
+    query2 = "select * from html where url='" + URL + "' and xpath='//table[contains(@class, \"hotel_outline_box\")]/tr[4]/td[2]/p'"
 
     #Preform the query and return the results to the object result
-    result = y.execute(query)
+    result1 = y.execute(query1)
+    result2 = y.execute(query2)
 
     #get the rows of the result (in our case, just 1) and give to rows
-    rows = result.rows
+    rows1 = result1.rows[0]
+    rows2 = result2.rows[0]
 
+    dict = {"5 star": rows1, "4 star": rows2}
     #return the only row.
-    return rows[0]
+    return dict
